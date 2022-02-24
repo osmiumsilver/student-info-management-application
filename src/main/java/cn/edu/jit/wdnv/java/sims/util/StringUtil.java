0d46c1cea3ee23612a6a1cea15587d06b476773d
osmiumsilver <169557899+osmiumsilver@users.noreply.github.com>
2022-02-24 11:59:08 +0800

    i changed a project maybe i've made it worse

--- src/main/java/cn/edu/jit/wdnv/java/sims/util/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.jit.wdnv.java.sims.util;
-
-public class StringUtil { //判断数据的真实空白
-    public static boolean isEmpty(String str) {
-        return "".equals(str) || str == null;
-    }
-}
